f3d366c297421e647a74d98a1a596046a90d4122
Vekktone <officialvekktone@gmail.com>
2019-05-07 20:03:42 -0500

    added other basic components

--- src/app/discussion/discussion.component.ts
+++ src/app/discussion/discussion.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Announcements} from '../announcements/announcements.component';
+import {FormsModule} from '@angular/forms';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-discussion',
   templateUrl: './discussion.component.html',
+  imports: [FormsModule],
   styleUrls: ['./discussion.component.css']
 })
 
 export class DiscussionComponent implements OnInit {
 
   userType: string;
+  subject: string;
+  body: string;
+  showMessage: boolean;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+    this.subject = '';
+    this.body = '';
+    this.showMessage = false;
+  }
 
   ngOnInit() {
     this.userType = this.route.snapshot.params['userType'];
 
+  }
+
+  myFunc() {
+    this.showMessage = true;
   }
 
 }
